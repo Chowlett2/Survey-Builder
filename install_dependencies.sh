18887825719650cfd6ce19abb98572b5ba4f5b30
Conor Howlett <55161121+Chowlett2@users.noreply.github.com>
2025-03-19 12:55:20 -0400

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,7 +2,7 @@
 
 # Install python3-distutils for missing distutils issue
 sudo apt-get update
-sudo apt-get install python3-distutils -y
+# sudo apt-get install python3-distutils -y
 
 # Install any additional dependencies, if necessary
 pip install -r requirements.txt
